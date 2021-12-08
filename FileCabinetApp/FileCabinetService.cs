d3583ea903a8f059c1316fd59b4adc0548aae51c
bovshis <bovhisdaniil@gmail.com>
2021-12-08 11:54:40 +0300

    Improve find firstname performance with dictionary.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace FileCabinetApp
     public class FileCabinetService
     {
         private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+        private readonly Dictionary<string, List<FileCabinetRecord>> firstNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short heigth, decimal weight, char favoriteCharacter)
         {
@@ -24,6 +26,7 @@ namespace FileCabinetApp
             };
 
             this.list.Add(record);
+            AddElementToDictionary(firstName.ToUpper(CultureInfo.InvariantCulture), record, this.firstNameDictionary);
 
             return record.Id;
         }
@@ -40,7 +43,7 @@ namespace FileCabinetApp
 
         public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, short heigth, decimal weight, char favoriteCharacter)
         {
-            this.list[id - 1] = new FileCabinetRecord
+            var record = new FileCabinetRecord
             {
                 Id = id,
                 FirstName = firstName,
@@ -50,14 +53,16 @@ namespace FileCabinetApp
                 Weight = weight,
                 FavoriteCharacter = favoriteCharacter,
             };
+            var oldRecord = this.list[id - 1];
+            this.list[id - 1] = record;
+
+            this.firstNameDictionary[oldRecord.FirstName.ToUpper(CultureInfo.InvariantCulture)].Remove(oldRecord);
+            AddElementToDictionary(firstName.ToUpper(CultureInfo.InvariantCulture), record, this.firstNameDictionary);
         }
 
         public FileCabinetRecord[] FindByFirstName(string firstName)
         {
-            var res = from p in this.list
-                      where p.FirstName.Equals(firstName, StringComparison.InvariantCultureIgnoreCase)
-                      select p;
-            return res.ToArray();
+            return this.firstNameDictionary[firstName.ToUpper(CultureInfo.InvariantCulture)].ToArray();
         }
 
         public FileCabinetRecord[] FindByLastName(string lastName)
@@ -74,6 +79,18 @@ namespace FileCabinetApp
                       where p.DateOfBirth.ToString("yyyy-MMM-dd").Equals(dateOfBirth, StringComparison.InvariantCultureIgnoreCase)
                       select p;
             return res.ToArray();
+        }
+
+        private static void AddElementToDictionary(string key, FileCabinetRecord record, Dictionary<string, List<FileCabinetRecord>> dictionary)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key].Add(record);
+            }
+            else
+            {
+                dictionary[key] = new List<FileCabinetRecord>() { record };
+            }
         }
     }
 }
